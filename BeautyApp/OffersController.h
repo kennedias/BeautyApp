6d733fb74fce03e51883ab73eabbad89f1ff297b
kennedias <kennedias@users.noreply.github.com>
2018-01-16 15:30:02 +1100

    nav

---
+++ BeautyApp/OffersController.h
@@ -0,0 +1,13 @@
+//
+//  OffersController.h
+//  BeautyApp
+//
+//  Created by 5399 on 16/1/18.
+//  Copyright © 2018 5399. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OffersController : NSObject
+
+@end
---
+++ BeautyApp/OffersController.m
@@ -0,0 +1,13 @@
+//
+//  OffersController.m
+//  BeautyApp
+//
+//  Created by 5399 on 16/1/18.
+//  Copyright © 2018 5399. All rights reserved.
+//
+
+#import "OffersController.h"
+
+@implementation OffersController
+
+@end
